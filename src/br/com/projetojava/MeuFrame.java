a6b3d1934fd89404f889cab76ac4181aa254c9cd
carolinezengo <carolinezengo@gmail.com>
2023-07-22 09:23:10 -0300

    Button

--- src/br/com/projetojava/MeuFrame.java
+++ src/br/com/projetojava/MeuFrame.java
@@ -1,15 +1,20 @@
 package br.com.projetojava;
 
+import javax.swing.JButton;
 import javax.swing.JFrame;
 
-public class MeuFrame {
+public class MeuFrame extends JFrame{
 	
 	public MeuFrame() {
-	JFrame frame = new JFrame();
+		
+		super ("Meu Frame");
+		
+	JButton botao = new JButton("Clique");
 	
-	frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-	frame.setSize(300,300);
-	frame.setVisible(true);
+          getContentPane().add(botao);
+	      setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+	      setSize(300,300);
+	      setVisible(true);
 }
 
 public static void main(String[] args) {
