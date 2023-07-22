091d5b158529bc1c97969608757c6202599bf6e5
carolinezengo <carolinezengo@gmail.com>
2023-07-22 10:02:52 -0300

    Button retornando mensagem

---
+++ src/br/com/projetojava/Eventos.java
@@ -0,0 +1,33 @@
+package br.com.projetojava;
+
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import javax.swing.JButton;
+import javax.swing.JFrame;
+
+public class Eventos extends JFrame implements ActionListener {
+	
+	public Eventos() {
+		
+		super("Meus eventos");
+		
+		JButton botao = new JButton("Clique");
+		botao.addActionListener(this);
+		
+		 getContentPane().add(botao);
+	      setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+	      setSize(300,300);
+	      setVisible(true);
+	}
+
+	public static void main(String[] args) {
+		new Eventos();
+		
+	}
+
+@Override
+public void actionPerformed(ActionEvent e) {
+	System.out.println("Você clicou!");	// TODO Auto-generated method stub
+	
+}
+}
