3b7afeafa83e5aa0e69b9f03125e4cddc34b5c12
carolinezengo <carolinezengo@gmail.com>
2023-07-22 09:36:50 -0300

    Button Container Norte e Sul

--- src/br/com/projetojava/Layout.java
+++ src/br/com/projetojava/Layout.java
@@ -1,5 +1,32 @@
 package br.com.projetojava;
 
-public class Layout {
+import javax.swing.JButton;
+import javax.swing.JFrame;
 
+import java.awt.BorderLayout;
+import java.awt.Container;
+
+public class Layout  extends JFrame {
+	public Layout() {
+		
+	
+	super ("Meu Layout");
+	
+	Container c = getContentPane(); 
+	c.add(BorderLayout.NORTH, new JButton("Botão Norte"));
+	c.add(BorderLayout.SOUTH, new JButton("Botao Sul"));
+	
+	
+	//getContentPane().add(new JButton("Botão Centralizado"));
+	
+	setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+    setSize(300,300);
+    setVisible(true);
+	
+	
+	}
+	
+	public static void main(String[] args) {
+		new Layout();
+	}
 }
